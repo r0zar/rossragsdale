67b966553ccb3fc5c385fad287e5f5f8668fc020
Ross Ragsdale <ross.ragsdale@gmail.com>
2021-02-20 15:41:59 -0600

    Adjust main text.

--- src/components/layout/_dom.js
+++ src/components/layout/_dom.js
@@ -16,15 +16,13 @@ const Dom = ({ dom }) => {
     <div className='absolute top-0 left-0 right-0 z-20 dom' {...events}>
       <Header />
       {dom}
-      <h1 className='absolute w-full text-2xl tracking-wider text-center text-white text-gray-100 md:mt-56 mt-28 top-1/2 sm:subpixel-antialiased md:antialiased'>
+      <h1 className='absolute w-full text-2xl tracking-wider text-center text-white md:mt-56 mt-28 top-1/2 sm:subpixel-antialiased md:antialiased'>
         Hi, I'm Ross.
       </h1>
-      <p className='absolute w-full pt-3 mt-32 text-xs tracking-wider text-center text-white text-gray-100 md:mt-64 top-1/2 sm:subpixel-antialiased md:antialiased'>
-        I'm a freelance web developer.
-      </p>
-      <p className='absolute w-full text-xs tracking-wider text-center text-white text-gray-100 md:mt-72 mt-36 top-1/2 sm:subpixel-antialiased md:antialiased'>
-        I build engaging, performant, full-stack web apps.
-      </p>
+      <div className='absolute w-full text-sm tracking-wider text-center text-gray-100 mt-36 md:mt-72 top-1/2 sm:subpixel-antialiased md:antialiased'>
+        <p>I'm a freelance web developer.</p>
+        <p>I build engaging, performant, full-stack web apps.</p>
+      </div>
     </div>
   )
 }
