fd2aac54f87dc87cde063987653c00e0de529700
Ross Ragsdale <ross.ragsdale@gmail.com>
2021-02-20 15:44:41 -0600

    Adjust main text.

--- src/components/layout/_dom.js
+++ src/components/layout/_dom.js
@@ -19,7 +19,7 @@ const Dom = ({ dom }) => {
       <h1 className='absolute w-full text-2xl tracking-wider text-center text-white md:mt-56 mt-28 top-1/2 sm:subpixel-antialiased md:antialiased'>
         Hi, I'm Ross.
       </h1>
-      <div className='absolute w-full text-sm tracking-wider text-center text-gray-100 mt-36 md:mt-72 top-1/2 sm:subpixel-antialiased md:antialiased'>
+      <div className='absolute w-full text-xs tracking-wider text-center text-gray-100 mt-36 md:mt-72 top-1/2 sm:subpixel-antialiased md:antialiased'>
         <p>I'm a freelance web developer.</p>
         <p>I build engaging, performant, full-stack web apps.</p>
       </div>
