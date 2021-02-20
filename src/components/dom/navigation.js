a5e53eaa77e7468bfc22ed9493a6651bd8f203e9
Ross Ragsdale <ross.ragsdale@gmail.com>
2021-02-20 15:57:19 -0600

    Clean up navigation.

--- src/components/dom/navigation.js
+++ src/components/dom/navigation.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const Navigation = () => {
   return (
-    <nav className='absolute text-xs right-4 top-4 xl:text-xl'>
+    <nav className='absolute text-xs xl:text-xl right-4 top-4'>
       <Link href='https://blog.rossragsdale.com'>
         <button className='relative z-20 p-2 m-2 text-white focus:outline-none focus:ring'>
           Blog
